05d3edc7f64ae19925cb2116f55b37836da3d99d
Monniasza <37037712+Monniasza@users.noreply.github.com>
2023-10-21 09:34:44 +0200

    imporved commands

---
+++ dictionaries/bug.py
@@ -0,0 +1,8 @@
+LONGEST_KEY = 1
+def lookup(chord):
+    if(chord[0] == "PWHROLG"):
+        raise IOError ("Your computer is not compatible with Plover")
+def reverse_lookup(text):
+    if(text == "kerbal"):
+        raise IOError ("Your computer is not compatible with KSP")
+    return []
